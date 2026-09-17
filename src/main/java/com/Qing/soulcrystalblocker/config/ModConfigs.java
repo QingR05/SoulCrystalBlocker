d3bc032a76283b3836081cfb2a36ac320e4de81a
QingR05 <jason2005yu@gmail.com>
2026-09-16 18:23:33 -0700

    fix config and change license to MIT

--- src/main/java/com/Qing/soulcrystalblocker/config/ModConfigs.java
+++ src/main/java/com/Qing/soulcrystalblocker/config/ModConfigs.java
@@ -12,19 +12,19 @@ public class ModConfigs {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 
         // Enable the mod setting
-        builder.comment("Prevent player dropping soul crystal setting").push("Preventing Player Dropping Soul Crystal Setting");
+        builder.comment("Prevent player dropping soul crystal setting").push("Dropping Soul Crystal Setting");
 
         PREVENT_DROP_ENABLE = builder
-                .comment("Enable/disable this function?")
-                .define("true to enable, preventing player dropping soul crystal. Else false to disable", true);
+                .comment("True to enable, preventing player dropping soul crystal, else disable")
+                .define("Enable/disable this function?", true);
 
         builder.pop();
 
-        builder.comment("Enable to bypass the limit to use The Architect's Favor").push("The Architect's Favor Setting");
+        builder.comment("Enable to bypass the limit to use The Architect's Favor").push("Bypass The Architect's Favor Setting");
 
         IS_THE_BLESSED_ONE_ENABLE = builder
-                .comment("Enable/disable this function?")
-                .define("true to enable, bypass the limit to use The Architect's Favor. Else false to disable", true);
+                .comment("True to enable, bypass the limit to use The Architect's Favor, else disable")
+                .define("Enable/disable this function?", true);
 
         SPEC = builder.build();
     }
